784c00800e908f3268c1f9d697cca21b5a18edf3
Nicolasinigo <ninigo93@gmail.com>
2025-10-03 20:31:25 -0300

    creamos estructura y controllers de usuarios

--- IñigoNicolas_61199/index.js
+++ IñigoNicolas_61199/index.js
@@ -2,13 +2,24 @@ require("dotenv").config()
 const express= require("express")
 const cors= require("cors")
 const conection= require("./config/db")
+const helmet = require("helmet")
 
 const app= express()
 
 app.use(express.json())
 app.use(cors())
+app.use(helmet())
 
 //ROUTES
 
-const PROT= process.env.PROT || 8000
+const PORT= process.env.PORT || 8000
 
+conection.connect((error)=>{
+    if (error) throw error
+    console.log("conectado a las base de datos")
+})
+
+app.listen(PORT, (error)=>{
+    if(error) throw error
+    console.log(`conectado a puerto ${PORT}`)
+})
