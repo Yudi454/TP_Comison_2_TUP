efba70cdca02ecf4e80df382fa3953a8860467aa
Luis Chrestia <chrestia.luis@gmail.com>
2025-10-10 00:06:46 -0300

    first commit

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mysql2 from 'mysql2/promise'
+
+const pool = mysql2.createPool({
+    host: 'localhost',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    multipleStatements: true
+});
+
+export default pool;
