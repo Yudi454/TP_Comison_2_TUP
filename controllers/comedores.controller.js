3673c04237ca31c4c91d60358d5a1a88f80b097e
NicoSox <nicosoxkij@gmail.com>
2025-10-10 20:22:25 -0300

    First commit

---
+++ controllers/comedores.controller.js
@@ -0,0 +1,75 @@
+import db from '../config/db.js';
+
+export const getAllComedores = async (req, res) => {
+  const query = `SELECT * FROM comedores`;
+
+  try {
+    const [results] = await db.query(query);
+    res.json(results);
+  } catch (err) {
+    console.error("Error al obtener los comedores", err);
+    res.status(500).json({ error: "Error al obtener comedores" });
+  }
+};
+
+
+export const getUnComedor = async (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT * FROM comedores WHERE id = ?`;
+
+  try {
+    const [results] = await db.query(query, [id]);
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: "Comedor no encontrado" });
+    }
+
+    res.json(results[0]);
+  } catch (err) {
+    console.error("Error al obtener el comedor", err);
+    res.status(500).json({ error: "Error al obtener el comedor" });
+  }
+};
+
+
+export const deleteUnComedor = async (req, res) => {
+  const { id } = req.params;
+  const query = `DELETE FROM comedores WHERE id = ?`;
+
+  try {
+    const [results] = await db.query(query, [id]);
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: "No fue posible dar de baja" });
+    }
+
+    res.json(results[0]);
+  } catch (err) {
+    console.error("Error al dar de baja", err);
+    res.status(500).json({ error: "Error al dar de baja" });
+  }
+};
+
+
+export const createNewComedor = async (req, res) => {
+  const { nombre, direccion, contacto, telefono } = req.body;
+
+  const query = `
+    INSERT INTO comedores (nombre, direccion, contacto, telefono)
+    VALUES (?, ?, ?, ?)
+  `;
+
+  try {
+    const [result] = await db.query(query, [nombre, direccion, contacto, telefono]);
+
+    res.status(201).json({
+      message: "Comedor creado correctamente",
+      comedorId: result.insertId
+    });
+  } catch (err) {
+    console.error("Error al dar de alta", err);
+    res.status(500).json({ error: "Error al dar de alta" });
+  }
+};
+
+
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import comedoresRoutes from './routes/comedores.route.js';
+
+console.log('USER:', process.env.MYSQL_USER);
+console.log('PASS:', process.env.MYSQL_PASS);
+console.log('DB:', process.env.MYSQL_DATABASE);
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+
+app.use('/comedores', comedoresRoutes);
+app.use('/comedores/:id', comedoresRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
