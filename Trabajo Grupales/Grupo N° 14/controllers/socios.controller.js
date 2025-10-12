5387e40a173feddc18d744ff4dfb8d11a8b64c71
Jeremias Alvarez <jeremiasal0209@gmail.com>
2025-10-12 19:21:58 -0300

    hecho edit o delete de usuarios - Jeremias

--- Trabajo Grupales/Grupo N° 14/controllers/socios.controller.js
+++ Trabajo Grupales/Grupo N° 14/controllers/socios.controller.js
@@ -29,4 +29,33 @@ const crearSocio = (req, res) => {
     });
 };
 
-module.exports = { getAllSocios, crearSocio };
+const modificarSocio = (req, res) => {
+    const { id } = req.params;
+    const { nombre, apellido, mail, telefono, fechaNacimiento } = req.body;
+    const consulta = "UPDATE socios SET nombre = ?, apellido = ?, mail = ?, telefono = ?, fechaNacimiento = ? WHERE idSocio = ?";
+    connection.query(consulta, [nombre, apellido, mail, telefono, fechaNacimiento, id], (error, results) => {
+        if (error) {
+            console.error("Error al modificar el socio:", error);
+            res.status(500).json({ error: "Error al modificar el socio" });
+        } else {
+            res.json({ message: "Socio modificado exitosamente" });
+        }
+    });
+
+}
+
+const eliminarSocio = (req, res) => {
+    const { id } = req.params;
+    const consulta = "DELETE FROM socios WHERE idSocio = ?";
+    connection.query(consulta, [id], (error, results) => {
+        if (error) {
+            console.error("Error al eliminar el socio:", error);
+            res.status(500).json({ error: "Error al eliminar el socio" });
+        } else {
+            res.json({ message: "Socio eliminado exitosamente" });
+        }
+    });
+}
+
+module.exports = { getAllSocios, crearSocio, modificarSocio,eliminarSocio};
+
--- Trabajo Grupales/Grupo N° 14/routes/socios.routes.js
+++ Trabajo Grupales/Grupo N° 14/routes/socios.routes.js
@@ -1,10 +1,13 @@
 const express = require('express'); 
 
-const { getAllSocios, crearSocio } = require('../controllers/socios.controller');
+const { getAllSocios, crearSocio, modificarSocio,eliminarSocio} = require('../controllers/socios.controller');
 
 const router = express.Router();
 
 router.get('/socios', getAllSocios);
 router.post('/socios/crearSocio', crearSocio);
+router.put('/socios/modificarSocio/:id', modificarSocio);
+router.delete('/socios/eliminarSocio/:id', eliminarSocio);
+
 
 module.exports = router;
