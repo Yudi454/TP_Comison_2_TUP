9e3bb1c7ffc047918a4064786471338a0b6eb1d2
Matias Garnica <victormatiasgarnica@gmail.com>
2025-10-12 18:46:02 -0300

    agregado de rutas en el index.js

--- Trabajo Grupales/Grupo N° 3/Registro de Actividades Culturales en una Municipalidad/back-end/index.js
+++ Trabajo Grupales/Grupo N° 3/Registro de Actividades Culturales en una Municipalidad/back-end/index.js
@@ -1,46 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
-const {connection} = require('./config/bd');
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//ROUTES RUTAS
-
+// RUTAS
 app.use('/api/eventos', require('./routes/eventos.routes'));
+app.use('/api/artistas-eventos', require('./routes/artistas_eventos.routes'));
 
-
-
-
-
-
-//RUTA PRINNCIPAL 
-
-
+// RUTA PRINCIPAL
 app.get('/', (req, res) => {
   res.json({ 
-    message: 'Bienvenido al Sistema de Gestión Comercial',
-    version: '1.0.0',
+    message: 'Bienvenido al Sistema de Gestión Cultural',
     endpoints: {
       eventos: '/api/eventos',
-
+      artistas_eventos: '/api/artistas-eventos'
     }
   });
 });
 
-
-
-
-//INICIAR SERVIDOR 
-
+// INICIAR SERVIDOR
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-console.log(`el servidor esta inciiado en el puerto ${PORT}`)
+  console.log(`El servidor está iniciado en el puerto ${PORT}`);
 });
-
-
 
 
