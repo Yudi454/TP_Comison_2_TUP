50edde0df4c398d15a1093ec09cedb8de6f64492
unknown <facubazan992009@gmail.com>
2025-10-13 20:29:20 -0300

    Empiezo a crear la estructura del backend - Bazan Facundo

--- SumerindeEmiliano_61557/README.md
+++
@@ -1,7 +0,0 @@
-# Entrega TP1
-
-**Alumno:** Sumerinde Emiliano  
-**Legajo:** 61557  
-**Carpeta:** SumerindeEmiliano_61557
-
-> en esta carpeta voy a ir subiendo todo lo que vayamos haciendo digamos
--- SumerindeEmiliano_61557/routes/metricas.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import * as ctrl from "../controllers/metricas.controller.js";
-const r = Router();
-r.get("/total-ventas", ctrl.totalVentas);
-r.get("/productos-mas-vendidos", ctrl.productosMasVendidos);
-r.get("/stock-bajo", ctrl.stockBajo);
-export default r;
