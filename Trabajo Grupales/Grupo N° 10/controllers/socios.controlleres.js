082000b1bc5c2f46747ff8b84f0d62cb60baf0eb
unknown <facubazan992009@gmail.com>
2025-10-13 21:43:36 -0300

    Termino de crear controlador de socios y empiezo con el de un solo socio - Bazan Facundo

--- Trabajo Grupales/Grupo N° 10/controllers/socios.controlleres.js
+++ Trabajo Grupales/Grupo N° 10/controllers/socios.controlleres.js
@@ -1,1 +1,14 @@
 const { conection } = require("../config/DB");
+const brcyptjs = require("bcryptjs")
+
+const getSocios = (req, res)=>{
+    const consulta = "select * from socios"
+
+    conection.query(consulta,(err,result)=>{
+        if(err){
+            console.log("Error al tratar de traer los socios", err)
+            return res.status(500).json({error:"Error al traer los socios"});
+        }
+        res.status(200).json({message:"Socios traidos con exito", result})
+    })
+}
