2b4758c6c57e9d0303a5df66fb870b5cfb40d157
ChocobarMatias <111932821+ChocobarMatias@users.noreply.github.com>
2025-10-17 04:33:38 -0300

    ultima entre del primer TP individual

--- SumerindeEmiliano_61557/routes/ventas.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as ctrl from "../controllers/ventas.controller.js";
-const r = Router();
-r.get("/", ctrl.listar);
-r.get("/:id", ctrl.obtener);
-r.post("/", ctrl.crear);
-r.delete("/:id", ctrl.eliminar);
-export default r;
