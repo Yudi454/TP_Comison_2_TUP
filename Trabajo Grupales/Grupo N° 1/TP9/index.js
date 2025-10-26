7217aef842dc38ab6a412fd7f6c2817c508845c9
Nahuel <nahueljuarez0707@gmail.com>
2025-10-25 23:15:22 -0300

    Agregada el encryptacion de contraseña

--- Trabajo Grupales/Grupo N° 1/TP9/index.js
+++ Trabajo Grupales/Grupo N° 1/TP9/index.js
@@ -4,24 +4,29 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+// Importar routers
 const sociosRoutes = require('./routes/socios.routes');
-// const deportesRoutes = require('./routes/deportes.routes');
-// const asignacionesRoutes = require('./routes/asignaciones.routes');
- const pagosRoutes = require('./routes/pagos.routes');
+const deportesRoutes = require('./routes/deportes.routes');
+const asignacionesRoutes = require('./routes/asignaciones.routes');
+const pagosRoutes = require('./routes/pagos.routes');
 
 const app = express();
+
+// Middlewares
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Rutas
 app.use('/api/socios', sociosRoutes);
 app.use('/api/deportes', deportesRoutes);
 app.use('/api/asignaciones', asignacionesRoutes);
-//app.use('/api/pagos', pagosRoutes);
+app.use('/api/pagos', pagosRoutes);
 
+// Ruta base
 app.get('/', (req, res) => res.json({ msg: 'API Club Deportivo OK' }));
 
-app.listen(process.env.PORT  , () =>
-  console.log(`Servidor activo en puerto ${process.env.PORT  }`)
-);
+// Puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor activo en puerto ${PORT}`));
